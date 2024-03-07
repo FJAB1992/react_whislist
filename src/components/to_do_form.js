46284710141a85f13e0fd3381e62247ea4354d44
FJAB1992 <fjarandabarba@gmail.com>
2024-03-07 08:45:40 +0100

    init01

--- src/components/to_do_form.js
+++ src/components/to_do_form.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const to_do = () => {
+export const to_do_form = () => {
     return (
         <div>
-            <h1>To do</h1>
+            <h1>To do Form</h1>
         </div>
     );
 };
--- src/components/to_do_wrapper.js
+++ src/components/to_do_wrapper.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const to_do = () => {
+export const to_do_wrapper = () => {
     return (
         <div>
-            <h1>To do</h1>
+            <h1>To do Wrapper</h1>
         </div>
     );
 };
